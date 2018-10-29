81f97c2f09a9a5bbc4f59366b95c8a0f4d84c31f
Lauren Snow <laursnowie@gmail.com>
2018-10-29 15:30:14 -0400

    cracking the code

--- js-drills-2.js
+++ js-drills-2.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function jediName (firstName, lastName) {
- return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
+  return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
 
 const result = jediName('Beyonce', 'Knowles');
@@ -10,18 +10,44 @@ console.log(result);
 
 const beyond = (num) => {
   if (num === Infinity){
-    console.log("And beyond")
+    console.log('And beyond');
   }
   if (num > 0 && num < Infinity){
-    console.log("To infinity")
+    console.log('To infinity');
   }
   if (num < 0 && num > Infinity){
-    console.log("To negative infinity")
+    console.log('To negative infinity');
   }
   if (num === 0){
-    console.log("Staying home")
+    console.log('Staying home');
   }
+
+};
+
+beyond(Infinity);
+
+function decode(word) {
+  if (word[0] === 'a') {
+    return word[1];
+  }
+  else if (word[0] === 'b') {
+    return word[2];
+  }
+  else if (word[0] === 'c') {
+    return word[3];
+  }
+  else if (word[0] === 'd') {
+    return word[4];
+  }
+  else {
+      return ' ';
+  }
+}
+
+function decodeString(string) {
+    return string.split(' ').map(word => decode(word)).join('');
 
 }
 
-beyond(Infinity);
+let codeBreaker = decodeString('craft block argon meter bells brown croon droop');
+console.log(codeBreaker);
